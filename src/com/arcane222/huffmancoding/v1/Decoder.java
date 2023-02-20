7f8b3070330ffb7f76064fcf7664085b2e1e328e
Lee Hong Jun <hong3883@naver.com>
2023-02-20 23:59:17 +0900

    Feat. add Decoder, Codec class
    - add Decoder, Codec class to separate the compression logic

---
+++ src/com/arcane222/huffmancoding/v1/Decoder.java
@@ -0,0 +1,12 @@
+package com.arcane222.huffmancoding.v1;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Decoder {
+
+    public Map<String, Character> decodingTable(File inputFile) {
+        return null;
+    }
+}
---
+++ src/com/arcane222/huffmancoding/v1/HuffmanCodec.java
@@ -0,0 +1,4 @@
+package com.arcane222.huffmancoding.v1;
+
+public class HuffmanCodec {
+}
